accadb5cb4678bbf46013ff5287319f2d44570b0
chenry900 <158115154+chenry900@users.noreply.github.com>
2025-02-05 15:27:26 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,26 @@
 function divideAndConquerSum(a) {
-    return a;
+    var sum = divideSum(a);
+    return sum;
+}
+function divideSum(a) {
+    if (a.length == 1)
+        return a[0];
+    if (a.length == 0)
+        return 0;
+    if (a.length == 2)
+    {
+        var firsthalf = a.slice(0,1);
+        var secondhalf = a.slice(1,2);
+        total = divideSum(firsthalf) + divideSum(secondhalf);
+        return total;
+    }
+    else {
+        var total = 0;
+        var b = Math.floor(a.length / 3);
+        var first = a.slice(0, b);
+        var second = a.slice(b, (b + b));
+        var last = a.slice((b + b), a.length);
+        total = divideSum(first) + divideSum(second) + divideSum(last);
+        return total;
+    }
 }
